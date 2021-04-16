f2182d4d794ee400a35f9d03469a741fc09671e8
Fer <fernandoza@gmail.com>
2021-04-16 18:03:10 -0500

    feat: setup test files

---
+++ js/api.test.js
@@ -0,0 +1,9 @@
+const postAPI = require('./postsAPI');
+
+test('get all posts', () => {
+    expect(1+2).toBe(3);
+    // function callback(data) => {
+    //     expect(data).toBe({"id": 1});
+    // }
+    // postAPI.get(1);
+});
