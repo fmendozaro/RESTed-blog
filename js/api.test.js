787f406efb9aacc7ea8a8db94b35cc1e0b69b832
Fer <fernandoza@gmail.com>
2021-04-18 18:07:11 -0500

    test: adds a get post test

--- js/api.test.js
+++ js/api.test.js
@@ -1,10 +1,14 @@
+'use strict';
 global.fetch = require("node-fetch");
 import postAPI from "./postsAPI";
+
+let tempId = 0;
 
 test('create a post', () => {
     return postAPI.create('test-title', 'test-body').then( (createdPost) => {
         expect(createdPost).not.toBeNull();
         expect(createdPost.id).toBeGreaterThan(0);
+        tempId = createdPost.id;
         expect(createdPost.title).toMatch('test-title');
         expect(createdPost.text).toMatch('test-body');
     });
@@ -13,5 +17,12 @@ test('create a post', () => {
 test('get all posts', () => {
     return postAPI.get().then( (posts) => {
         expect(posts.length).toBeGreaterThan(0);
+    });
+});
+
+test('get a post', () => {
+    return postAPI.get(tempId).then( post => {
+        expect(post).toBeDefined();
+        expect(post.id).toBe(tempId);
     });
 });
